f1e3da43bc62474a71670e1631c7baa17511d7c1
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-05-29 23:41:02 +0200

    fix hero title typo

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -5,7 +5,7 @@
     <section>
         <div class="container mb-20 mt-10 md:mb-40 md:mt-20">
             <p class="md:text-xl text-center">Hi, I am Nick,</p>
-            <h1 class="hero-title my-4 text-center">Freelance <span class="text-brand-red">Web Develeper</span></h1>
+            <h1 class="hero-title my-4 text-center">Freelance <span class="text-brand-red">Web Developer</span></h1>
             <div class="md:text-center md:text-xl flex flex-col gap-3">
                 <p>I create pixel-perfect, fast, and SEO-friendly business websites and landing pages.</p>
                 <p>I specialize in websites for restaurants, bars, and cafes.</p>
