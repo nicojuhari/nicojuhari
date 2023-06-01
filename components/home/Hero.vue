c2135df4b3562c62478d13dda998b9f01242d159
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-06-01 10:46:56 +0200

    adds img to my services section

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -2,11 +2,9 @@
 
 </script>
 <template>
-    <div class="container my-10 md:my-32">
-                <p class="md:text-xl text-center">Hi, I am Nick,</p>
-                <h1 class="hero-title my-4 text-center">Freelance <span class="text-brand-red">Web Developer</span></h1>
-                <div class="text-center md:text-xl flex flex-col gap-3">
-                    <h2>I create pixel-perfect, fast, and SEO-friendly websites and landing pages.</h2>
-                </div>
+    <div class="container my-10 md:my-32 text-xl text-center">
+                <p>Hi, I am Nick,</p>
+                <h1 class="hero-title my-4">Freelance <span class="text-brand-red">Web Developer</span></h1>
+                <h2>I create pixel-perfect, fast, and SEO-friendly websites and landing pages.</h2>
             </div>
 </template>
--- components/home/MyServices.vue
+++ components/home/MyServices.vue
@@ -4,17 +4,17 @@
         { 
             title: 'Restaurant website',
             description: 'Excellent website for increasing sales and attracting new customers for your restaurant.',
-            icon: 'ic:round-restaurant-menu'
+            icon: 'restaurant-website.png'
         },
         {
             title: 'Business website',
             description: 'Great website for your company to demonstrate its strength and generate leads.',
-            icon: 'ic:round-business-center'
+            icon: 'business-website.png'
         },
         {
         title: 'Maintenance & Support',
         description: 'I will fix any JavaScript, CSS, or HTML error, website updates, and other minor tasks.',
-        icon: 'ic:round-handyman'
+        icon: 'maintenance-support.png'
     }
     ]
 </script>
@@ -22,11 +22,13 @@
     <section>
         <div class="container">
             <div class="subtitle my-10 text-center">Services</div>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <div v-for="item in myServices" class="flex flex-col gap-6 p-6 rounded-xl card-box-shadow hover:-translate-y-3 duration-700 ">
-                    <Icon :name="item.icon" class="w-12 h-12 text-brand-red m-auto"/>
-                    <h3 class="font-medium text-center text-2xl text-brand-color">{{ item.title }}</h3>
-                    <p class="text-lg font-light text-center">{{ item.description }}</p>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-20 md:gap-6 pt-20">
+                <div v-for="item in myServices" class=" p-6 rounded-xl card-box-shadow hover:-translate-y-3 duration-700">
+                    <div class="flex flex-col gap-6 -mt-20">
+                        <img :src="'/' + item.icon" :alt="item.title" :title="item.title" class="h-40 m-auto"/>
+                        <h3 class="font-medium text-center text-2xl text-brand-color">{{ item.title }}</h3>
+                        <p class="text-lg font-light text-center">{{ item.description }}</p>
+                    </div>
                 </div>
             </div>
         </div>
