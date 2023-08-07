bbaca87ea6f7bde4f14aff69d450a0452c0d820d
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-08-07 12:26:39 +0300

    moved restaurant demos to 1food.menu

--- pages/demos/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    const demos = useDemos();
-</script>
-<template>
-    <div class="container">
-        <div class="subtitle my-10 text-center">Demos</div>
-        <projects-demos/>
-    </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -19,14 +19,6 @@
             <div class="container">
                 <div class="subtitle my-10 text-center">Projects</div>
                 <ProjectsList :projects_limit=3 :view_all_projects=true />
-                </div>
-            </section>
-        <section>
-            <div class="container">
-                <div class="subtitle my-10 text-center">Demo Projects</div>
-                <ProjectsDemos/>
-                <NuxtLink to="/demos" title="View All Demos" class="btn btn-secondary mx-auto mt-10">
-                View All Demos</NuxtLink>
             </div>
         </section>
     </div>
