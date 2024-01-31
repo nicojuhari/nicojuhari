32984d9379a639b4594294b9232a9c62f3761dc0
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-01-31 21:44:47 +0100

    remove ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@formkit/nuxt', '@vueuse/nuxt', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
