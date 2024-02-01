b909154802068c328ab9d71e1bc4a4c863b9359a
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-02-01 22:16:02 +0100

    refactor home hero section

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -5,6 +5,6 @@
     <div class="container py-24 md:py-44  md:text-2xl font-light">
         <p>Hi, I am Nick,</p>
         <h1 class="hero-title my-6 text-brand-blue">Freelance Web Developer</h1>
-        <h2>I create pixel-perfect, fast, and SEO-friendly websites and landing pages.</h2>
+        <h2>I create pixel-perfect, fast, and SEO-friendly <a href="https://apps.nicojuhari.com/" class="underline-offset-2  underline">web apps</a></h2>
     </div>
 </template>
--- components/includes/Header.vue
+++ components/includes/Header.vue
@@ -3,13 +3,11 @@
 </script>
 <template>
     <header class="bg-brand-bg sticky top-0 z-10 bg-white bg-opacity-[0.025] backdrop-blur-md border-b border-white border-opacity-[0.05]">
-        <div class="container py-2">
-            <div class="flex justify-between items-center gap-4">
-                <NuxtLink to="/" title="Ideal Credit">
-                    <IconsLogo class="w-8 text-brand-gray" />
-                </NuxtLink>
-                <a href="https://forms.gle/UX2VPKUwyyFdSXbw9" class="btn btn-outline btn-light" target="blank" title="Hire me">Hire me</a>
-            </div>
+        <div class="container flex justify-between items-center gap-4 h-12 sm:h-14 py-2">
+            <NuxtLink to="/" title="Ideal Credit">
+                <IconsLogo class="h-8 sm:h-10 text-brand-gray" />
+            </NuxtLink>
+            <!-- <UButton to="https://forms.gle/UX2VPKUwyyFdSXbw9" variant="outline" color="brand-blue" target="blank" title="Hire me">Hire me</UButton> -->
         </div>
     </header>
 </template>
