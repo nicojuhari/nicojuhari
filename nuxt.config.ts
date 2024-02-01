ace98ada03df3b17c755f1e04a405369da979a6d
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-02-01 15:12:56 +0100

    adds default website title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,34 @@ export default defineNuxtConfig({
   routeRules: {
     '/free-menu-maker/**': { ssr: false },
   },
-   runtimeConfig: {
+  runtimeConfig: {
         public : {
             gtag: {
                 id: 'G-YC2EDZRT9H'
             },
+            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://apps.nicojuhari.com',
+            siteName: 'Apps by Freelance Web Developer',
+            siteDescription: 'I create pixel-perfect, fast, and SEO-friendly websites and landing pages. I specialize in websites for restaurants, bars, and cafes.',
+            language: 'en', 
+        },
+    },
+  app: {
+        head: { 
+            htmlAttrs: {
+                    lang: 'en'
+            },
+            title: 'Apps by Freelance Web Developer',
+            link: [
+                { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
+                // { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
+                // { rel: 'shortcut icon', type:'image/png', sizes:"16x16", href:"/favicon-16x16.png" },
+                // { rel: 'apple-touch-icon', sizes:"180x180", href:"/apple-touch-icon.png" }
+            ],
+            meta: [
+                { name: 'description', content: 'I create pixel-perfect, fast, and SEO-friendly websites and landing pages. I specialize in websites for restaurants, bars, and cafes.' },
+                { name: 'keywords', content: 'Web developer, Freelancer, Vienna Austria, restaurant websites, website developer' },
+                { name: 'google-site-verification', content: "oOiepCGY8_h_x8LguPX3bbyO0MKDXRKE6dSSKlSUiKI"}
+            ],
         },
     },
 })
