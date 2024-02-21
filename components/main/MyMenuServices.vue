e50784d070ec6e4dac133282277f28a754b168c6
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-02-21 21:29:12 +0100

    customize menu maker page

--- components/main/MyMenuServices.vue
+++ components/main/MyMenuServices.vue
@@ -17,8 +17,7 @@
 
 </script>
 <template>
-    <div class="bg-brand-950 p-10 md:p-12 text-white">
-        
+    <div class="bg-brand-950 p-6 md:p-12 text-white">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div class="grid grid-cols-1 gap-6">
                 <div v-for="item in list" :key="item.title" class="flex items-center gap-4 text-gray-100">
@@ -29,9 +28,9 @@
                     </div>
                 </div>
             </div>
-            <div class="grid place-content-center">
+            <div class="grid place-content-center -order-1 md:order-2">
                 <h3 class="text-3xl font-bold mb-6">Do you need more?</h3>
-                <div class="text-center mt-4">
+                <div class="text-center my-6">
                     <UButton to="/contact" size="lg" color="brand" class="m-sauto" title="Contact me">Contact me</UButton>
                 </div>
             </div>
