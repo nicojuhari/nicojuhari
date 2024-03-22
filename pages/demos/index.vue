052adbf40d545ca32aa81ea7e84798f70e80aeda
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-03-22 11:21:33 +0100

    adds 5th demo

--- pages/demos/index.vue
+++ pages/demos/index.vue
@@ -6,7 +6,7 @@ definePageMeta({
 const list = [
     {
         url: 1,
-        title: 'Italian Restaurant',
+        title: 'Trattoria Bella Italia',
     },
     {
         url: 2,
@@ -19,6 +19,10 @@ const list = [
     {
         url: 4,
         title: 'Tony\'s Restaurant',
+    },
+    {
+        url: 5,
+        title: 'Bella Cucina Restaurant',
     }
 ]
 </script>
@@ -29,8 +33,8 @@ const list = [
                 <h1 class="page-title">Restaurant Website Design Examples</h1>
                 <div class="text-xl mt-2">From simple to complex designs</div>
             </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10 md:mt-16 gap-8">
-                <NuxtLink :to="'/demos/restaurant-website-design/'+ item.url" class="p-6 border bg-slate-50 rounded-md text-xl font-medium" v-for="item in list">{{ item.title }}</NuxtLink>
+            <div class="grid grid-cols-1 sm:grid-cols-2 mt-10 md:mt-16 gap-8">
+                <NuxtLink :to="'/demos/restaurant-website-design/'+ item.url" target="_blank" class="p-6 border bg-slate-50 rounded-md text-xl font-medium" v-for="item in list">{{ item.title }}</NuxtLink>
             </div>
         </div>
     </section>
---
+++ pages/demos/restaurant-website-design/5.vue
@@ -0,0 +1,240 @@
+<script setup>
+import { computed, ref, onMounted } from 'vue';
+
+import gsap from "gsap";
+
+// get other plugins:
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+
+// const menuURL = 'https://api.1food.menu/v1/menus/l9zwcpvlanynrmthvmb'
+
+// const { data: menuData } = await useFetch(menuURL)
+import menuData from '@/assets/json/demo-menu.json'
+
+const onefmCategory = ref(null)
+const showModal = ref(false)
+const productForModal = ref(null)
+const selectedCategory = ref('')
+const filteredCategories = ref(menuData?.categories || [])
+const filteredProducts = ref(menuData?.products || [])
+
+const productsByCategory = computed(() => {
+    let groupedProducts = {};
+
+    filteredProducts.value.forEach(product => {
+        if (!groupedProducts[product.categoryId]) {
+            groupedProducts[product.categoryId] = [];
+        }
+        groupedProducts[product.categoryId].push(product);
+    });
+
+    return groupedProducts;
+})
+
+const filterCategories = (category_id) => {
+
+    if (!category_id) {
+        selectedCategory.value = ''
+        filteredCategories.value = [...menuData.categories]
+        return
+    }
+    selectedCategory.value = category_id
+    filteredCategories.value = [...menuData.categories].filter(category => category.uid == category_id)
+}
+
+const openModal = (product) => {
+    showModal.value = true
+    productForModal.value = product
+}
+
+const closeModal = () => {
+    showModal.value = false
+    productForModal.value = null
+}
+
+useHead({
+    title: 'Menu Design 2',
+    meta: [
+        { name: 'keywords', content: 'menu design, menu templates, restaurant website design, digital food menu' },
+        { name: 'description', content: 'Menu Design 2, Responsive Template' }
+    ],
+})
+
+useHead({
+    title: 'Restaurant Website Design Example - Bella Cucina Restaurant',
+    bodyAttrs: {
+        class: 'bg-slate-50'
+    },
+    meta: [
+        { name: 'keywords', content: 'demo website, restaurant website design, digital food menu' },
+        { name: 'description', content: `Demo website of Bella Cucina Restaurant, modern and clean design. Created with LOVE using Nuxt.js, TailwindCSS and Free Menu Maker` },
+    ],
+})
+
+onMounted(() => {
+    gsap.registerPlugin(ScrollTrigger)
+    onefmCategory.value.forEach(category => {
+        gsap.fromTo(category.children, { autoAlpha: 0, y: 40 }, {
+            scrollTrigger: {
+                trigger: category,
+                start: "20px 95%",
+
+                // markers: true,
+            },
+            autoAlpha: 1,
+            y: 0,
+            stagger: 0.3,
+            duration: .8,
+        })
+    })
+
+})
+
+</script>
+<template>
+    <div>
+        <div class="bg-white">
+            <div class="container grid grid-cols-1 md:grid-cols-2 !w-full md:py-4">
+                <img src="https://images.pexels.com/photos/696218/pexels-photo-696218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    class="h-72 object-cover w-full md:rounded-md" />
+                <div class="p-4 flex flex-col">
+                    <h1 class="title font-bold">Bella Cucina Restaurant</h1>
+                    <div class="flex flex-col gap-2 relative py-4">
+                        <div class="flex gap-2">
+                            <div class="text-green-500 font-bold">Open</div>
+                            <span>&#183;</span>
+                            <div>Closes at 22:00</div>
+                        </div>
+                        <div class="flex gap-2">
+                            <div>Cash & Card</div>
+                            <span>&#183;</span>
+                            <div>City, 1st Street, nr. 34, 856479</div>
+                        </div>
+                    </div>
+                    <div
+                        class="flex gap-4 items-center mt-10 md:mt-auto px-4 py-2 bg-green-500 bg-opacity-10 rounded-full max-w-max md:ml-0 mx-auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 text-green-500 flex-shrink-0 font-bold">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                        </svg>
+                        <div class="text-lg font-semibold text-green-500 cursor-pointer">0123 456 7890</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-white border-t border-b border-gray-600 border-opacity-10 sticky top-0 z-10">
+            <div class="container py-2">
+                <div class="overflow-x-auto py-2 px-1 text-sm">
+                    <div class="flex gap-4 mx-auto">
+
+                        <div v-for="category in [{ uid: '', name: 'View All' }, ...menuData.categories]"
+                            :key="category.uid" @click="filterCategories(category.uid)"
+                            :class="{ '!bg-gray-700 bg-opacity-70 text-white': selectedCategory == category.uid }"
+                            class="bg-white border py-2 px-4 rounded-full cursor-pointer flex-shrink-0 duration-500"> {{
+                            category.name }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="menu-products min-h-96">
+                <template v-for="category in filteredCategories">
+                    <template v-if="productsByCategory[category.uid]">
+                        <div class="text-2xl mt-6 font-semibold mb-2">
+                            {{ category.name }}
+                        </div>
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 pb-6 pt-2" ref="onefmCategory">
+                            <div v-for="product in productsByCategory[category.uid]" @click="openModal(product)"
+                                class="flex cursor-pointer rounded-xl  bg-white flex-shrink-0 snap-start shadow-lg h-40">
+
+                                <div
+                                    class="h-full w-40 image-bg image-bg-2 shrink-0 border-b border-opacity-50 rounded-l-xl">
+                                    <div class="image-bg h-full w-full rounded-l-xl"
+                                        :style="`background-image: url(${product.imageUrl})`">
+                                    </div>
+                                </div>
+                                <div class="p-4 flex flex-col flex-grow">
+                                    <div class="font-bold mb-2">{{ product.name }}</div>
+                                    <div class="opacity-60 leading-none my-2 line-clamp-2 w-full">{{ product.description
+                                        }}</div>
+                                    <div class="flex justify-between mt-auto pt-2">
+                                        <div class="text-sm text-gray-400"> {{ product.options?.[0]?.size }} </div>
+                                        <div class="font-bold flex gap-2 items-center">
+                                            <div v-if="product.options?.[0]?.salePrice" class="text-green-700">$ {{
+                                                product.options?.[0]?.salePrice }}</div>
+                                            <div class="text-green-700"
+                                                :class="{ '!text-red-600 line-through': product.options?.[0]?.salePrice }">
+                                                $
+                                                {{ product.options?.[0]?.price }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                </template>
+                <div v-if="!Object.keys(productsByCategory).length" class="py-10 text-center text-2xl">
+                    Sorry, no products found!
+                </div>
+
+            </div>
+            <UModal v-model="showModal" :ui="{ width: 'sm:max-w-[375px]' }">
+                <div class="flex flex-col bg-white flex-shrink-0 rounded-xl overflow-hidden relative">
+                    <UButton square color="gray" variant="soft" icon="i-ph-x-bold"
+                        class="absolute rounded-full bg-opacity-60 right-3 top-3 font-bold text-gray-950"
+                        @click="showModal = false" />
+                    <div class="h-80 w-full image-bg image-bg-2 shrink-0 border-b border-opacity-50">
+                        <div class="image-bg h-full w-full rounded-t-xl"
+                            :style="`background-image: url(${productForModal.imageUrl})`">
+                        </div>
+                    </div>
+                    <div class="p-4 flex flex-col flex-grow">
+                        <div class="font-bold my-2">{{ productForModal.name }}</div>
+                        <div v-if="productForModal.tags" class="flex gap-2 overflow-x-auto">
+                            <div v-for="tag in productForModal.tags"
+                                class="text-xs px-2 py-1 rounded-full bg-green-600 bg-opacity-10 text-green-600 flex-shrink-0">
+                                {{ tag
+                                }}</div>
+                        </div>
+                        <div class="text-gray-600 my-2">{{ productForModal.description }}</div>
+
+                        <div v-if="productForModal.options">
+                            <div v-for="item in productForModal.options" class="flex justify-between mt-auto pt-2">
+                                <div class="text-sm text-gray-400">
+                                    {{ item.size }}</div>
+                                <div class="flex gap-2 items-center">
+                                    <div v-if="item.salePrice" class="text-gray-700">$ {{ item.salePrice }}</div>
+                                    <div class="text-gray-700"
+                                        :class="{ '!text-red-600 line-through': item.salePrice }">$
+                                        {{ item.price }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </UModal>
+        </div>
+        <footer class="border-t border-r-gray-100">
+            <div class="container py-6 flex gap-6 items-center flex-col md:flex-row md:justify-between">
+                <div class="font-bold">Bella Cucina</div>
+                <div>Created with &lt;3 by <NuxtLink to="/" class="font-bold">me</NuxtLink>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+<style>
+.image-bg,
+.image-bg-2 {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.image-bg-2 {
+    background-image: url('https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image-300x225.png');
+    background-color: rgba(231, 231, 231, 0.217);
+}
+</style>
