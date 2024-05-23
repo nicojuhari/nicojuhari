3fc0c1b223838e8eaefeff1499544d39e6af0153
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-05-23 10:38:49 +0200

    improve the qr code genrator page

--- components/menu/ToolDescription.vue
+++ components/menu/ToolDescription.vue
@@ -39,16 +39,16 @@
             modern,
             interactive way to showcase your culinary creations and enhance the customer experience.</p>
 
-        <h3 class="text-2xl pt-4">What is a Digital Menu?</h3>
+        <h3 class="text-2xl pt-4 font-semibold">What is a Digital Menu?</h3>
 
         <p>A digital menu is an electronic version of your traditional paper menu, typically displayed on a tablet or
             your
-            restaurant's website. It allows you to present your food and drink offerings in a visually appealing and
+            restaurant's website.<br>It allows you to present your food and drink offerings in a visually appealing and
             interactive format.</p>
 
-        <h3 class="text-2xl pt-4">Why Go Digital? The Benefits of a Digital Menu:</h3>
+        <h3 class="text-2xl pt-4 font-semibold">Why Go Digital? The Benefits of a Digital Menu:</h3>
 
-        <ul class="list-inside list-disc">
+        <ul class="list-inside list-disc space-y-1.5">
             <li><strong>Improved Customer Experience:</strong> Digital menus are easier to navigate, offering features
                 like
                 zooming in on photos or filtering items by category.</li>
@@ -66,12 +66,12 @@
                 platforms).</li>
         </ul>
 
-        <h3 class="text-2xl pt-4">Why a Digital Menu is Better Than a PDF Menu:</h3>
+        <h3 class="text-2xl pt-4 font-semibold">Why a Digital Menu is Better Than a PDF Menu:</h3>
 
         <p>While PDF menus might seem like a convenient option, they lack the flexibility and interactivity of a digital
             menu. Here's why digital trumps PDF:</p>
 
-        <ul class="list-inside list-disc">
+        <ul class="list-inside list-disc space-y-1.5">
             <li><strong>Limited Functionality:</strong> PDF menus are static documents with no interactive features.
             </li>
             <li><strong>Not Mobile-Friendly:</strong> Viewing a PDF menu on a mobile device can be cumbersome.</li>
@@ -80,11 +80,11 @@
             <li><strong>No Analytics:</strong> PDF menus don't provide any data insights on customer behavior.</li>
         </ul>
 
-        <h3 class="text-2xl pt-4">How to Enhance Your Website with a Digital Menu Maker:</h3>
+        <h3 class="text-2xl pt-4 font-semibold">How to Enhance Your Website with a Digital Menu Maker:</h3>
 
         <p>Here's how a free digital menu maker can help you elevate your restaurant's website:</p>
 
-        <ul class="list-inside list-disc">
+        <ul class="list-inside list-disc space-y-1.5">
             <li><strong>Easy to Use:</strong> Most free digital menu makers offer user-friendly interfaces for easy menu
                 creation.</li>
             <li><strong>Customization Options:</strong> Choose from a variety of templates and design options to match
@@ -95,30 +95,31 @@
             </li>
         </ul>
 
-        <h3 class="text-2xl pt-10">Free Digital Menu Maker: FAQs:</h3>
+        <h3 class="text-2xl pt-4 font-semibold">Free Digital Menu Maker: FAQs:</h3>
 
         <p>Here are some frequently asked questions about digital menus and websites:</p>
 
-        <h4 class="text-lg pt-4">Does a digital menu replace my website?</h4>
+        <div class="text-lg pt-4 font-bold">Does a digital menu replace my website?</div>
 
         <p>No, your digital menu should be integrated into your existing website. Think of it as an interactive element
             that enhances your online presence.</p>
 
-        <h4 class="text-lg pt-4">How do customers access the digital menu?</h4>
+        <div class="text-lg pt-4 font-bold">How do customers access the digital menu?</div>
 
-        <p>Several options exist. You can display the menu directly on your website or provide a QR code that customers
+        <p>Several options exist. You can display the menu directly on your website or provide a QR code that
+            customers
             can scan with their smartphones to access it.</p>
 
-        <h4 class="text-lg pt-4">Can I update my digital menu frequently?</h4>
+        <div class="text-lg pt-4 font-bold">Can I update my digital menu frequently?</div>
 
         <p>Absolutely! One of the biggest advantages of a digital menu is the ease of making updates. You can change
             prices, add or remove items, and update descriptions instantly.</p>
 
-        <h4 class="text-lg pt-4">What are some additional features to look for in a digital menu maker?</h4>
+        <div class="text-lg pt-4 font-bold">What are some additional features to look for in a digital menu maker?</div>
 
         <p>While free options offer a great starting point, some menu makers offer paid plans with advanced features
             like:</p>
-        <ul class="list-disc list-inside space-y-1 my-6">
+        <ul class="list-disc list-inside space-y-1.5 my-6">
             <li>Custom branding options</li>
             <li>Advanced analytics and reporting</li>
             <li>Online ordering and payment integration</li>
@@ -126,7 +127,7 @@
             <li>Integration with your Point-of-Sale (POS) system</li>
         </ul>
 
-        <p class="pt-8"><strong>In conclusion</strong></p>
+        <div class="text-lg pt-8 font-bold">In conclusion:</div>
         <p>
             In the digital world of today, a free digital menu creator can be an extremely useful tool for any
             restaurant, bar, or cafe owner.
