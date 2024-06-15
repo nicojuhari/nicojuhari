d2ba408f6c9faea865e5174e004bb87e6e630544
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-06-15 16:47:11 +0200

    removes app list from menu maker page

--- pages/tools.vue
+++ pages/tools.vue
@@ -1,9 +1,11 @@
 <script setup>
+    const route = useRoute()
 
+    //if we have a query parameter, we will redirect to the page with the query parameter
 </script>
 <template>
     <NuxtPage />
-    <MainAppsList>
+    <MainAppsList v-if="!route.query.menu_uid">
         <template #sectionTitle v-if="$route.path != '/tools'">
             <h3 class="subtitle mb-10 text-center">Other tools you may like</h3>
         </template>
