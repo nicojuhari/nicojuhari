e2b6493fb884cd843e6b199539ac91e9de159a7c
Nicolae Cojuhari <nicojuhari@gmail.com>
2025-02-12 09:10:39 +0100

    removes bunny cdn from local and adds the external link, to the app

---
+++ pages/tools/bunny-cdn.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+    useHead({
+        title: 'Bunny CDN asset management',
+    })
+
+</script>   
+<template>
+    <div class="container py-10">
+        <div class="text-center my-20">
+            <div class="text-2xl font-bold">Sorry, the app is moved to a new place.</div>
+            <div class="text-lg mt-8">Please, use <a class="text-blue-500 underline" href="https://bunny-cdn.netlify.app/" target="_blank">Bunny CDN</a> instead.</div>
+        </div>
+    </div>
+</template>
